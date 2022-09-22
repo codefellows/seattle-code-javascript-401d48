4a23544e4b7cc8b8553b002cb64ae20c2f348ed0
Ryan Gallaway <rkgallaway@msn.com>
2022-09-21 18:36:34 -0700

    logger tests added

---
+++ class-02/inclass-demo/src/__tests__/logger.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let logger = require('../middleware/logger');
+
+describe('Test Logger Middleware', () => {
+  let consoleSpy;
+  let req = {};
+  let res = {};
+  let next = jest.fn(); // spy on the next method
+
+  beforeEach(() => {
+    // Attach to the console (spy on it or take it over)
+    consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+  });
+  afterEach(() => {
+    // put things back to normal.  stop spying
+    consoleSpy.mockRestore();
+  });
+
+  test('Properly logs output', () => {
+    logger(req, res, next);
+    console.log('req',req);
+    expect(consoleSpy).toHaveBeenCalledWith(`REQUEST: ${req.method}, ${req.originalUrl}`);
+  });
+  test('Properly calls next()', () => {
+    logger(req, res, next);
+    expect(next).toHaveBeenCalledWith();
+  });
+});
