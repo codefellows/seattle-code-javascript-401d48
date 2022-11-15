1bc47b5467cc839cc9261447b6a0a6043fc3c8ea
Ryan Gallaway <rkgallaway@msn.com>
2022-11-15 12:05:13 -0800

    class-assets

---
+++ class-43/LECTURE.md
@@ -0,0 +1,7 @@
+# Frameworks
+
+- svelte - we can get and display input on page!
+- vue
+- gatsby - SEO placement
+- next - some fun backend capability - server side rendering / built in API
+- Nuxt (vue based) ???  curious about this
---
+++ class-43/inclass-demo/vue/src/components/Input.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      avg: '',
+      min: '',
+      max:'',
+    }
+  }
+}
+</script>
+
+<template>
+  <p>Store Name: {{ name }}</p>
+	<input v-model="name" placeholder="edit me" />
+  <p>Average Cookies Per Hour: {{ avg }}</p>
+	<input v-model="avg" placeholder="edit me" />
+  <p>Minimum: {{ min }}</p>
+	<input v-model="min" placeholder="edit me" />
+  <p>Maximum: {{ max }}</p>
+	<input v-model="max" placeholder="edit me" />
+
+</template>
